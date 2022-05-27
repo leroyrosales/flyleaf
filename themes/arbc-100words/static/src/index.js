e25ab8857ec2133a76cda8779fa594998c11fd50
leroyrosales <leroyrosales@gmail.com>
2022-05-27 14:29:07 -0500

    Toggles view of dropdown elements

--- themes/arbc-100words/static/src/index.js
+++ themes/arbc-100words/static/src/index.js
@@ -6,4 +6,8 @@ const header = document.getElementById('header');
 const firstSection = document.querySelector('main section');
 header.style.backgroundColor = window.getComputedStyle(firstSection).backgroundColor;
 
+const categoryFilter = document.getElementById('category-filter');
 
+categoryFilter.addEventListener( 'click', (e) => 
+    e.target.nextSibling.nextSibling.classList.toggle('hidden') 
+);
