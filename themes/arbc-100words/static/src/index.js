f193bce3f29890c406526c0df0f1d1306e327b55
leroyrosales <leroyrosales@gmail.com>
2022-05-27 14:43:29 -0500

    Updates to form for filtering

--- themes/arbc-100words/static/src/index.js
+++ themes/arbc-100words/static/src/index.js
@@ -9,5 +9,13 @@ header.style.backgroundColor = window.getComputedStyle(firstSection).backgroundC
 const categoryFilter = document.getElementById('category-filter');
 
 categoryFilter.addEventListener( 'click', (e) => 
-    e.target.nextSibling.nextSibling.classList.toggle('hidden') 
+    e.target.nextSibling.nextSibling.classList.toggle('hidden')
 );
+
+const storyCategories = document.querySelectorAll('[data-category]');
+
+for( let i = 0; i < storyCategories.length; i++ ) {
+    storyCategories[i].addEventListener('click', () => {
+        console.log( storyCategories[i].dataset.category )
+    })
+}
