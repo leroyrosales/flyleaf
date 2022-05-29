b1a0306da7c844b722c967333415ff3d5d36c446
Leroy Rosales <leroyrosales@gmail.com>
2022-05-28 22:41:09 -0500

    Init confirmation

--- themes/arbc-100words/functions/gravity-forms.php
+++ themes/arbc-100words/functions/gravity-forms.php
@@ -18,12 +18,25 @@ function els_load_scripts() {
 add_action( 'wp_enqueue_scripts', 'els_load_scripts' );
 
 add_filter( 'gform_submit_button', 'arbc_add_span_tags', 10, 2 );
-function arbc_add_span_tags ( $button, $form ) {
+function arbc_add_span_tags( $button, $form ) {
 
 	return $button .= "<span aria-hidden='true'></span>";
 
 }
 
-add_filter('acf/format_value/type=text', 'do_shortcode');
+add_filter( 'acf/format_value/type=text', 'do_shortcode' );
 
 
+add_filter( 'gform_confirmation', 'arbc_custom_confirmation', 10, 4 );
+function arbc_custom_confirmation( $confirmation, $form, $entry, $ajax ) {
+
+
+
+	$confirmation = '<section class="one-hundred-form_wrapper">
+	<h2>Thank you for sharing your story!</h2>
+	<p><em>Your story will be posted after our review.</em></p>
+	<a href="#">Share another story</a> <span class="pen-nav-item"><a href="/stories">View Stories</a></span>
+</section>';
+
+	return $confirmation;
+}
