5b75c25f9081dab9eea055844f9cf7a2259eb478
Leroy Rosales <leroyrosales@gmail.com>
2022-05-31 20:09:35 -0500

    Fixes category filter

--- themes/arbc-100words/static/src/index.js
+++ themes/arbc-100words/static/src/index.js
@@ -2,13 +2,13 @@ import './main.css';
 
 "use strict";
 
-const header = document.getElementById('header');
-const firstSection = document.querySelector('main section');
-header.style.backgroundColor = window.getComputedStyle(firstSection).backgroundColor;
+// const header = document.getElementById('header');
+// const firstSection = document.querySelector('main section');
+// header.style.backgroundColor = window.getComputedStyle(firstSection).backgroundColor;
 
 const categoryFilter = document.getElementById('category-filter');
 
-categoryFilter.addEventListener( 'click', (e) => 
+categoryFilter.addEventListener( 'click', (e) =>
     e.target.nextSibling.nextSibling.classList.toggle('hidden')
 );
 
