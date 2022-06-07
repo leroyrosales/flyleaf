6fcae27999e1462b92c0cad00136b192bfd2cb5b
Leroy Rosales <leroyrosales@gmail.com>
2022-06-07 14:54:57 -0500

    Mobile updates

--- themes/arbc-100words/static/src/index.js
+++ themes/arbc-100words/static/src/index.js
@@ -6,16 +6,12 @@ import './main.css';
 // const firstSection = document.querySelector('main section');
 // header.style.backgroundColor = window.getComputedStyle(firstSection).backgroundColor;
 
-const categoryFilter = document.getElementById('category-filter');
-
-categoryFilter.addEventListener( 'click', (e) =>
-    e.target.nextSibling.nextSibling.classList.toggle('hidden')
-);
+const categoryMenu = document.querySelector( '[data-category-menu]' );
 
 document.addEventListener( 'click', function (e) {
 
-	if ( e.target.matches( '[data-category]' ) ) {
-		console.log(e.target.dataset.category);
+	if ( e.target.matches( '[data-menu-btn]' ) ) {
+		categoryMenu.classList.toggle( 'hidden' );
 	}
 
 }, false);
