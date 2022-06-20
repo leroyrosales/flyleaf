85d02d344efdbca0103b1987a5a4ccea59428f44
leroyrosales <leroyrosales@gmail.com>
2022-06-19 22:36:16 -0500

    Completes mobile nav and fixes desktop nav

--- themes/arbc-100words/static/src/index.js
+++ themes/arbc-100words/static/src/index.js
@@ -7,21 +7,48 @@ import './main.css';
 // header.style.backgroundColor = window.getComputedStyle(firstSection).backgroundColor;
 
 // const categoryMenu = document.querySelector( '[data-category-menu]' );
-const mobileMenu = document.querySelector( '[data-mobile-menu]' );
+const openMobileMenu = document.querySelector( '[data-mobile-menu-open]' );
+const closeMobileMenu = document.querySelector( '[data-mobile-menu-close]' );
 const mobileNav = document.querySelector( '[data-mobile-nav]' );
 const desktopNav = document.getElementById( 'navbar' );
 
-document.addEventListener( 'click', function (e) {
+// document.addEventListener( 'click', function (e) {
 
-	if ( e.target.matches( '[data-menu-btn]' ) ) {
-		//categoryMenu.classList.toggle( 'hidden' );
-	}
+// 	if ( e.target.matches( '[data-menu-btn]' ) ) {
+// 		//categoryMenu.classList.toggle( 'hidden' );
+// 	}
 
-}, false);
+// }, false);
 
-mobileMenu.addEventListener( 'click', () => {
+openMobileMenu.addEventListener( 'click', () => {
 	//mobileNav.classList.toggle( 'hidden' );
 	mobileNav.style.display = mobileNav.style.display === 'none' ? '' : 'none';
 	desktopNav.style.display = 'none';
 });
 
+closeMobileMenu.addEventListener( 'click', () => {
+	//mobileNav.classList.toggle( 'hidden' );
+	mobileNav.style.display = mobileNav.style.display === 'none' ? '' : 'none';
+	desktopNav.style.display = 'flex';
+});
+
+// Filtering categories
+const categoryMenu = document.querySelector( '[data-category-menu]' );
+
+document.addEventListener( 'click', function (e) {
+
+	if ( e.target.matches( '[data-menu-btn]' ) ) {
+		categoryMenu.classList.toggle( 'hidden' );
+	}
+
+	// if ( e.target.matches( '[data-close-btn]' ) ) {
+	// 	mobileNav.style.display = mobileNav.style.display === 'none' ? '' : 'none';
+	// 	desktopNav.style.display = 'flex';
+	// }
+
+	// if ( e.target.matches( '[data-mobile-menu]' ) ) {
+	// 	mobileNav.style.display = mobileNav.style.display === 'none' ? '' : 'none';
+	// 	desktopNav.style.display = 'none';
+	// }
+
+}, false);
