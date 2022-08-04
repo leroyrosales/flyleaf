5869bb95b92285fbceacbb3497a9251e4c97412a
Leroy Rosales <leroyrosales@gmail.com>
2022-08-04 10:16:16 -0500

    Rewrite some queries

--- themes/arbc-100words/functions/timber-context.php
+++ themes/arbc-100words/functions/timber-context.php
@@ -21,40 +21,50 @@ add_filter( 'timber_context', function ( $context ) {
 
 	// Multiple queries
 	/******  The First Query *******/
-	$first_stories = new WP_Query( array(
+	$first_stories_arr = array(
 		'post_type'     => 'story',
 		'post_per_page' => 10,
 		'orderby'       => 'rand',
-	) );
+	);
 
-	$context['first_stories'] = new Timber\PostQuery( $first_stories );
+	$first_stories = new Timber\PostQuery( $first_stories_arr );
+
+	$context['first_stories'] = $first_stories;
 
 	$exclude = wp_list_pluck( $first_stories, 'ID' );
 
 	/******  The Second Query *******/
-	$second_stories = new WP_Query( array(
+	$second_stories_arr = array(
 		'post_type'      => 'story',
 		'post__not_in'   => $exclude, // Tell WordPress to Exclude these posts
 		'posts_per_page' => 10,
 		'orderby'        => 'rand',
-	) );
+	);
 
-	$context['second_stories'] = new Timber\PostQuery( $second_stories );
+	$second_stories = new Timber\PostQuery( $second_stories_arr );
+
+	$context['second_stories'] = $second_stories;
+
 
 	$combined_stories = new WP_Query();
-	$combined_stories->posts = array_merge( $first_stories->posts, $second_stories->posts );
+	$combined_stories->posts = array_merge(
+		json_decode( json_encode( $first_stories ), true ),
+		json_decode( json_encode( $second_stories ), true )
+	);
 
 	$exclude_more = wp_list_pluck( $combined_stories->posts, 'ID' );
 
-	/******  The Second Query *******/
-	$third_stories = new WP_Query( array(
+	// /******  The Third Query *******/
+	$third_stories_arr = array(
 		'post_type'      => 'story',
-		'post__not_in'   => $exclude_more, // Tell WordPress to Exclude these posts
+		'post__not_in'   =>  $exclude_more, // Tell WordPress to Exclude these posts
 		'posts_per_page' => 10,
 		'orderby'        => 'rand',
-	) );
+	);
 
-	$context['third_stories'] = new Timber\PostQuery( $third_stories );
+	$third_stories = new Timber\PostQuery( $third_stories_arr );
+
+	$context['third_stories'] = $third_stories;
 
 	$context['first_stories_page']  = is_page_template( 'page-first-stories.php' );
 	$context['second_stories_page'] = is_page_template( 'page-second-stories.php' );
